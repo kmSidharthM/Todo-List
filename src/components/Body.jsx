d3110ccdbda8ea343c0273b23e19e47a1e1e4d08
kmSidharthM <kmsidharthmurali@gmail.com>
2024-07-07 02:25:17 +0530

    added localstorage support

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -3,12 +3,17 @@ import TodoContainer from "./TodoContainer";
 import TodoInput from "./TodoInput";
 
 const Body = () => {
-  const [allTasks, setAllTasks] = useState([]);
-  const [key, setKey] = useState(0);
+  const [allTasks, setAllTasks] = useState(JSON.parse(localStorage.getItem("todoList")) || []);
+  const [key, setKey] = useState(JSON.parse(localStorage.getItem("key")) || 0);
 
   const addTask = (task) => {
-    setAllTasks([...allTasks, {id: key ,task, isActive: true}]);
-    setKey(key + 1);
+    const modifiedTasks = [...allTasks, {id: key ,task, isActive: true}];
+    setAllTasks(modifiedTasks);
+    localStorage.setItem("todoList", JSON.stringify(modifiedTasks));
+    // console.log(localStorage.getItem("todoList"));
+    const updatedKey = key + 1;
+    setKey(updatedKey);
+    localStorage.setItem("key", JSON.stringify(updatedKey));
     // console.log(allTasks);
   }
   const isTaskActive = (id) => {
@@ -18,15 +23,23 @@ const Body = () => {
       return task;
     })
     setAllTasks(modifiedTasks);
+    localStorage.setItem("todoList", JSON.stringify(modifiedTasks));
   }
 
   const deleteTask = (id) => {
     const modifiedTasks = allTasks.filter(task => task.id != id)
     setAllTasks(modifiedTasks);
+    localStorage.setItem("todoList", JSON.stringify(modifiedTasks));
+    if(modifiedTasks.length === 0) {
+      setKey(0);
+      localStorage.setItem("key", 0);
+    }
+        
   }
 
   return ( 
     <div className="flex flex-col items-center">
+      <p>{Date()}</p>
       <TodoInput addTask={addTask}/>
       <TodoContainer allTasks={allTasks} isTaskActive={isTaskActive} deleteTask={deleteTask} />
     </div>
--- src/components/TodoItems.jsx
+++ src/components/TodoItems.jsx
@@ -3,10 +3,7 @@ import DeleteIcon from '@mui/icons-material/Delete';
 
 
 const TodoItems = ({item, isTaskActive, deleteTask}) => {
-const [isChecked, setIsChecked] = useState(false);
-
 const handleChange = (event) => {
-  setIsChecked(event.target.checked);
   isTaskActive(item.id);
 }
 
@@ -16,7 +13,7 @@ const handleClick = () => {
 
   return ( 
     <div className="flex gap-4 p-2 border-b-2 rounded-md items-center m-4 ">
-      <input type="checkbox" checked={isChecked} onChange={handleChange}/>
+      <input type="checkbox" checked={!item.isActive} onChange={handleChange}/>
       <p className={ `w-60 pr-2 overflow-hidden text-ellipsis ${item.isActive ? "no-underline" : "line-through"}` } >{item.task}</p>
       <button onClick={handleClick}><DeleteIcon sx={{ color: "mediumpurple" }} /></button>
     </div>
